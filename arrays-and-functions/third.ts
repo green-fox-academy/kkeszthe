9721bb9c090185960707d8016f6f19685039b50e
Katalin Keszthelyi <kkeszthe@gmail.com>
2019-11-09 11:24:58 +0100

    Added missing comma

--- arrays-and-functions/third.ts
+++ arrays-and-functions/third.ts
@@ -3,4 +3,4 @@
 //    with the following content: `[1, 3, 5, 7]`
 // -  Print the third element of `magicNumbers`
 let magicNumbers: number[] = [1, 3, 5, 7];
-console.log(magicNumbers[2])
+console.log(magicNumbers[2]);
