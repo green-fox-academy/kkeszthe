9fd8d300e6f0b302646504607bc81bdb697ec0d8
Katalin Keszthelyi <kkeszthe@gmail.com>
2019-11-09 16:00:03 +0100

    Corrected swap-elements.ts code

--- arrays-and-functions/swap-elements.ts
+++ arrays-and-functions/swap-elements.ts
@@ -2,6 +2,6 @@
 // -  Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
 // -  Swap the first and the third element of `abc`
 let abc: string[] = ["Arthur", "Boe", "Chloe"];
-let cba: string[] = ["Arthur", "Boe", "Chloe"];
+let cba: string[] = [...abc];
 abc[0] = cba[2];
 abc[2] = cba [0];
