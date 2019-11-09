82988a24897c3f95e48ce6ff5faf89a919291025
Katalin Keszthelyi <kkeszthe@gmail.com>
2019-11-09 12:32:20 +0100

    Added a to all animal names

---
+++ arrays-and-functions/append-a.ts
@@ -0,0 +1,8 @@
+"use strict"
+// -  Create an array variable named `animals`
+//    with the following content: `["koal", "pand", "zebr"]`
+// -  Add all elements an `"a"` at the end
+let animals: string[] = ["koal", "pand", "zebr"];
+for (let i = 0; i < animals.length; i++){
+    animals[i] = animals[i]+"a";
+}
