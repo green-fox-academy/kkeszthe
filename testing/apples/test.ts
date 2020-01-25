fef7aa9249318f45d774e904645ab6e70e7994e0
Katalin Keszthelyi <kkeszthe@gmail.com>
2020-01-25 12:12:21 +0100

    Apple excercise done

--- testing/apples/test.ts
+++ testing/apples/test.ts
@@ -1,10 +1,10 @@
 import { Apple } from './Apples';
-import {test} from '../node_modules/tape';
+import {test} from './node_modules/tape';
 
 test('Initiate Apple class', t => {
     let v: Apple = new Apple
 
     t.equal(v.getApple(), 'apple');
     t.end()
-})
+});
 
