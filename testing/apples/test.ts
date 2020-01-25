6e9e79066c25be4b0f1ce097bdb8e78c7bc4989d
Katalin Keszthelyi <kkeszthe@gmail.com>
2020-01-25 11:57:12 +0100

    removing npm

---
+++ testing/apples/test.ts
@@ -0,0 +1,10 @@
+import { Apple } from './Apples';
+import {test} from '../node_modules/tape';
+
+test('Initiate Apple class', t => {
+    let v: Apple = new Apple
+
+    t.equal(v.getApple(), 'apple');
+    t.end()
+})
+
