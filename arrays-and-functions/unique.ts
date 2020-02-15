020dfc9943bcbe5a50d37bc6c3f74c222ca62535
Katalin Keszthelyi <kkeszthe@gmail.com>
2020-02-15 08:10:04 +0100

    Changed active code

--- arrays-and-functions/unique.ts
+++ arrays-and-functions/unique.ts
@@ -1,7 +1,7 @@
 //  Create a function that takes a list of numbers as a parameter
 //  Returns a list of numbers where every number in the list occurs only once
 
-/* function unique(arr: number[]): number[] {
+function unique(arr: number[]): number[] {
    const uniArr: number[] = [];
    for(let i = 0; i < arr.length; i++){
        
@@ -10,33 +10,32 @@
        else{
            uniArr.push(arr[i]);
         } 
-   
    }  
    return uniArr
     
 }
-// */
 
-function unique(arr: number[]): number[] {
-    const uniArr: number[] = [];
-    for(let i = 0; i < arr.length; i++){
-        let test: boolean = false;
-        for(let j = 0; j < uniArr.length || j < 1; j++){
-           if(arr[i]==uniArr[j]) {
-               test = test || true;
-           }
+
+// function unique(arr: number[]): number[] {
+//     const uniArr: number[] = [];
+//     for(let i = 0; i < arr.length; i++){
+//         let test: boolean = false;
+//         for(let j = 0; j < uniArr.length || j < 1; j++){
+//            if(arr[i]==uniArr[j]) {
+//                test = test || true;
+//            }
             
-        }
-        if(test == true) {
-        }
-        else{
-            uniArr.push(arr[i]);
-        }
+//         }
+//         if(test == true) {
+//         }
+//         else{
+//             uniArr.push(arr[i]);
+//         }
        
-    }
-    return uniArr
+//     }
+//     return uniArr
      
- }
+//  }
 //  Example
-console.log(unique([1, 11, 34, 11, 52, 61, 1, 34]))
+//console.log(unique([1, 11, 34, 11, 52, 61, 1, 34]))
 //  should print: `[1, 11, 34, 52, 61]'
