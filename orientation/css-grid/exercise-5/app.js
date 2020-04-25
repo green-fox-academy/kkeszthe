66ca0e2747a48affef75e4e0e8de03f7068e499b
Katalin Keszthelyi <kkeszthe@gmail.com>
2020-04-25 14:50:16 +0200

    CSS Gridd exercises done

---
+++ orientation/css-grid/exercise-5/app.js
@@ -0,0 +1,6 @@
+for (let i = 0; i < 20; i++) {
+	const body = document.querySelector('body');
+	const div = document.createElement('div');
+	div.setAttribute('class', 'field');
+	body.appendChild(div);
+}
